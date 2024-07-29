a61ae3cae2580169f3fc78ce5c279cc2bef0049c
johnLamberts <faithsacredoo3@gmail.com>
2024-07-30 00:04:28 +0800

    testing the db con, not official implementation

--- app-management-server/src/server.ts
+++ app-management-server/src/server.ts
@@ -1,1 +1,19 @@
-console.log("Test me mout")
+
+import colors from "colors.ts";
+import { connectoDB } from "@/common/database";
+import express, { Application } from "express";
+
+colors?.enable();
+
+
+const app: Application = express();
+
+// ----------------------------------------------------
+// Establish Connection
+
+connectoDB(process?.env.MONGODB_URL as string, process?.env.MONGODB_COLLECTION_NAME as string)
+
+// ----------------------------------------------------
+
+
+export default app;
